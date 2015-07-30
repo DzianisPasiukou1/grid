2fd27d058f26df53b68b257a67528e4e9bfae18e
DzianisPasiukou1 <Dzianis_Pasiukou1@epam.com>
2015-07-30 19:17:48 +0300

    Changed select2 inputs, added multiple inputs.

--- GridAzure/app/directives/page-content-d3/page-content-d3.js
+++ GridAzure/app/directives/page-content-d3/page-content-d3.js
@@ -15,26 +15,74 @@
 				var initializer = new Initializer(scope, element, content, templatesPath, $compile);
 				initializer.initCards();
 
-				scope.contentOptions.eventType = { options: { actions: [{ label: 'Simple event' }, { label: 'Medium event' }] }, selectOpt: {} };
-				scope.contentOptions.segments = { options: { actions: [{ label: 'All segments' }, { label: 'First segment' }, { label: 'Second segment' }] }, selectOpt: {} };
-				scope.contentOptions.campaign = { options: { actions: [{ label: 'All campaigns' }, { label: 'First campaign' }, { label: 'Second campaign' }] }, selectOpt: {} };
+				scope.contentOptions.eventType = {
+					options: {
+						actions: [
+							{ label: 'Simple event' },
+							{ label: 'Medium event' }],
+						selected: { label: 'Simple event' }
+					},
+					selectOpt: {}
+				};
+				scope.contentOptions.segments = {
+					options: {
+						actions: [{ label: 'All segments', isAll: true }, { label: 'First segment' }, { label: 'Second segment' }],
+						onChange: function () {
+							//if (Array.isArray(this.selected)) {
+							//	if (this.selected.length > 1) {
+							//		for (var select in this.selected) {
+							//			if (JSON.parse(this.selected[select]).isAll) {
+							//				if (select == this.selected.length - 1) {
+							//					this.selected = this.selected.filter(function (value) {
+							//						if (JSON.parse(value).isAll) {
+							//							return value;
+							//						}
+							//					})
+							//				}
+							//				else {
+							//					this.selected = this.selected.filter(function (value) {
+							//						if (!JSON.parse(value).isAll) {
+							//							return value;
+							//						}
+							//					})
+							//				}
+							//				break;
+							//			}
+							//		}
+							//	}
+							//}
+						},
+						onSelect: function (action) {
+							this.lastSelect = action;
+						}
+					},
+					selectOpt: {}
+				};
+				scope.contentOptions.campaign = {
+					options: {
+						actions: [{ label: 'All campaigns', isAll: true }, { label: 'First campaign' }, { label: 'Second campaign' }],
+						onChange: function () {
+							if (Array.isArray(this.selected)) {
+
+							}
+						}
+					},
+					selectOpt: {}
+				};
 
 				scope.filters = {
 					dateRange: {
 						start: moment(new Date(new Date().setDate(new Date().getDate() - 7))),
 						end: moment(new Date())
-					}
-				};
-
-				scope.$watch('cardsOptions.dateRange', function (date) {
-					if (date) {
+					},
+					onDateRangeChange: function () {
 						for (var card in scope.cardsOptions.cards) {
 							if (scope.cardsOptions.cards[card].counter) {
-								scope.cardsOptions.cards[card].count = scope.cardsOptions.cards[card].counter.calculate(scope.cardsOptions.startDate, scope.cardsOptions.endDate);
+								scope.cardsOptions.cards[card].count = scope.cardsOptions.cards[card].counter.calculate(this.dateRange.start.toDate(), this.dateRange.end.toDate());
 							}
 						}
 					}
-				});
+				};
 			}
 		};
 	}])
