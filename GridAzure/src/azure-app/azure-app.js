316d50aef64b0df0c208540665f4f6d20bce309c
DzianisPasiukou1 <Dzianis_Pasiukou1@epam.com>
2015-08-20 21:28:16 +0300

    Creating release folder

---
+++ GridAzure/src/azure-app/azure-app.js
@@ -0,0 +1,1 @@
+﻿angular.module('azureApp', ['gridTaskApp', 'ngRoute']);
---
+++ GridAzure/src/test/unit/directives/chart-segment/chartSegmentSpec.js
@@ -0,0 +1,11 @@
+﻿describe('Chart Segment Rendering', function () {
+	var element, elementScope, scope;
+
+	beforeEach(module('gridTaskApp'));
+	beforeEach(inject(function ($compile, $rootScope) {
+		scope = $rootScope.$new();
+		scope.selectedUsers = {};
+
+		element = compile('<chart-segment selected-users="selectedUsers"></chart-segment>')(scope);
+	}))
+});
