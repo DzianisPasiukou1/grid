4439982bd8d1e399ae7c6ce930e411061e435be6
DzianisPasiukou1 <Dzianis_Pasiukou1@epam.com>
2015-08-21 22:11:52 +0300

    edited downloads

--- dev/server.js
+++ dev/server.js
@@ -6,6 +6,7 @@ var app = express();
 
 app.use('/src' , express.static(__dirname + '/src'));
 app.use('/dist' , express.static(__dirname + '/dist'));
+app.use('/release' , express.static(__dirname + '/release'));
 
 app.get('/', function (req, res) {
 	res.sendfile(path.join(__dirname, 'dist/index.html'));
